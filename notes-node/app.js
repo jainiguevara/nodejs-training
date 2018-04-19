f0ff26d5805d1f91ad119b2c539642846ea7f111
Jaini Guevara <jaini.guevara@gmail.com>
2018-04-19 17:51:42 +0800

    #41918 Project complete - Notes v1.0

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,16 +1,36 @@
-console.log("Starting app.js");
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
 
-var argv = yargs.argv;
-var command = yargs.argv._[0];
+//command options
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+};
 
-console.log('Command: ', command);
-console.log('Yargs', argv);
+var argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+var command = yargs.argv._[0];
 
 // add arguement
 if (command === 'add') {
@@ -23,7 +43,9 @@ if (command === 'add') {
         console.log(`Duplicate title: ${argv.title}`);
 
 } else if  (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((e) => notes.logNote(e));
 } else if  (command === 'read') {
     var note = notes.readNote(argv.title);
 
